4161438b6a32850ccf285d4ef9606fae6f1c392a
Andreea Ardelean <andreea@Andreeas-MBP.lan>
2016-10-20 20:23:48 +0100

    Creating the prototype for the renew journey
    From after the photo upload till the passport options pages

--- routes/renew/steps.js
+++ routes/renew/steps.js
@@ -1,5 +1,45 @@
 module.exports = {
     '/': {
-        next: '/continue'
+        fields: ['passport-number', 'expiry-year', 'expiry-month'],
+        next: '/check-personal-data'
+    },
+    '/check-personal-data': {
+        next: '/check-photo'
+    },
+    '/check-photo':{
+        next: '/title'
+    },
+    '/title':{
+        fields: ['title'],
+        next: '/name'
+    },
+    '/name':{
+        fields: ['name','lastname'],
+        next: '/gender'
+    },
+    '/gender':{
+        next: '/date-and-place-birth'
+    },
+    '/date-and-place-birth':{
+        fields:['age-day', 'age-month', 'age-year', 'town-birth'],
+        next: '/home-address'
+    },
+    '/home-address':{
+        fields:['address1', 'address2', 'town', 'postcode'],
+        next: '/contact-details'
+    },
+    '/contact-details':{
+        fields:['email', 'mobile'],
+        next: '/get-updates'
+    },
+    '/get-updates':{
+        next: '/passport-options'
+    },
+    '/passport-options':{
+        fields:['passport-options'],
+        next: '/delivering'
+    },
+    '/delivering':{
+        next: '/title'
     }
 };
