6521ee885d20236460f1c004b89c9cca51f7df40
Gavin Boulton <gavin.boulton@homeoffice.gov.uk>
2016-11-08 12:38:02 +0000

    Update next steps and backlinks for photoguide-short
    When a `next` property of a step is set the backlink can be automatically determined. These values can be used in templates as `{{backLink}}` and `{{nextPage}}`.

--- routes/prototype_161110/photoguide-short/steps.js
+++ routes/prototype_161110/photoguide-short/steps.js
@@ -1,24 +1,24 @@
 module.exports = {
-    '/': {
-      backLink: '/../overseas/choose-photo-method'
-    },
-    '/short-find-camera': {
-      backLink: './choose-photo-method'
-    },
-    '/short-plain-bg': {
-      backLink: '/short-find-camera'
-    },
-    '/short-right-position': {
-      backLink: '/short-plain-bg'
-    },
-    '/short-lighting': {
-      backLink: '/short-right-position'
-    },
-    '/photorules-icao': {
-      backLink: '/short-lighting'
-    },
-    '/short-upload-photo': {
-      backlink: '/short-quality'
-    }
-    
+  '/': {
+    next: '/short-find-camera',
+    backLink: '/../overseas/choose-photo-method'
+  },
+  '/short-find-camera': {
+    next: '/short-plain-bg',
+    backLink: './'
+  },
+  '/short-plain-bg': {
+    next: '/short-right-position'
+  },
+  '/short-right-position': {
+    next: '/short-lighting'
+  },
+  '/short-lighting': {
+    next: '/photorules-icao'
+  },
+  '/photorules-icao': {
+    next: '/short-upload-photo'
+  },
+  '/short-upload-photo': {
+  }
 };
