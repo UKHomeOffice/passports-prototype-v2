9c0585399feb7ea2d5adde562532accf104ea123
Andreea Ardelean <andreea@Andreeas-MacBook-Pro.local>
2016-11-30 11:31:18 +0000

    Redirected to different folders according to the country decision
    If you are in the UK you continue with the UK filter
    If you are from overseas you continue with the Overseas filter

--- routes/prototype_161212/filter-common/steps.js
+++ routes/prototype_161212/filter-common/steps.js
@@ -1,11 +1,9 @@
 module.exports = {
     '/':{
       controller: require('../../../controllers/application-country'),
-      controller: require('../../../controllers/go-overseas'),/* added this here so if you answer yes you are applying from the uk to redirect you to the index of the UK filter */
       fields: ['apply-uk', 'application-country'],
-      backLink: '/../prototype_161212/startpage-overseas',
-      next: '/../filter', /* if Yes is selected */
-      nextAlt: '../overseas/what-do-you-want-to-do' /* added this here so if you answer yes you are applying from the uk to redirect you to the index of the UK filter */
+      backLink: '/../prototype_161212/startpage',
+      next: '/what-do-you-want-to-do', /* if Yes is selected */
     },
     '/what-do-you-want-to-do': {
         fields: ['what-to-do'],
@@ -14,7 +12,9 @@ module.exports = {
     },
     '/dob': {
       fields: ['age-day', 'age-year', 'age-month'],
-        backLink: './',
-        next: '/passport-damaged' /* if Yes is selected */
+      controller: require('../../../controllers/go-overseas'),
+      backLink: './',
+      next: '/../filter', /* if they are from the UK */
+      nextAlt: '../overseas' /* if they are from overseas */
     }
 };
