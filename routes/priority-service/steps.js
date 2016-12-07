d717310e40d2a2aaa151e9d49680bf71c9153a6d
John-Henry Barac <johnhenrybarac@mac.com>
2016-12-07 09:53:25 +0000

    added prices page

--- routes/priority-service/steps.js
+++ routes/priority-service/steps.js
@@ -3,6 +3,9 @@ module.exports = {
         template: 'index',
         next: '/what-to-take'
     },
+    '/prices': {
+        backLink: './'
+    },
     '/what-to-take': {
         backLink: './'
     }
