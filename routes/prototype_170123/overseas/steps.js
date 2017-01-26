fd30d8d03b4414fdbc597bc9dd7e34d2b2e7898b
Andreea Ardelean <andreea@Andreeas-MacBook-Pro.local>
2017-01-26 15:15:48 +0000

    Back link fix on issuing info

--- routes/prototype_170123/overseas/steps.js
+++ routes/prototype_170123/overseas/steps.js
@@ -1,7 +1,7 @@
 module.exports = {
     '/':{
       fields: ['issuing-authority', 'issue-year', 'issue-month'],
-      backLink: '../filter-common/dob',
+      backLink: '/../prototype_170123/filter-common/dob-overseas',
       next: '/passport-damaged'
     },
     '/passport-damaged': {
