5f85e308481b217f21d739e945bacfe3defe6ea0
Rebecca Cottrell <r.e.cottrell@gmail.com>
2017-02-03 13:31:25 +0000

    Add placeholder prototype for next iteration

---
+++ routes/priority_service_170215/filter/steps.js
@@ -0,0 +1,19 @@
+module.exports = {
+    '/': {
+      fields: ['expiry-year', 'expiry-month'],
+      backLink: '../filter-common/dob',
+      next: '/passport-damaged'
+    },
+    '/passport-damaged': {
+      fields: ['passport-damaged'],
+        backLink: './',
+        next: '/uncancelled' /* if No is selected */
+    },
+    '/uncancelled': {
+        controller: require('../../../controllers/go-overseas'),
+        fields: ['uncancelled', 'dual-national-country'],
+        backLink: './passport-damaged',
+        next: '/../intro',
+        nextAlt: '../overseas'
+    }
+};
---
+++ routes/priority_service_170215/renew/steps.js
@@ -0,0 +1,93 @@
+module.exports = {
+    '/':{
+        backLink: '../uploadphoto/check-photo-and-submit',
+        next: '/old-pass-details'
+    },
+    '/old-pass-details': {
+        backLink: './',
+        fields: ['passport-number', 'expiry-year', 'expiry-month'],
+        next: '/title'
+    },
+    '/title':{
+        fields: ['title'],
+        next: '/name'
+    },
+    '/name':{
+        fields: [
+            'name',
+            'lastname',
+            'previous-name',
+            'previous-names'
+        ],
+        next: '/gender'
+    },
+    '/gender':{
+        fields: ['gender'],
+        next: '/date-and-place-birth'
+    },
+    '/date-and-place-birth':{
+        next: '/home-address',
+        fields:['age-day', 'age-month', 'age-year', 'born-in-uk', 'town-of-birth', 'country-of-birth'],
+        controller: require('../../../controllers/go-overseas'),
+        nextAlt: './home-address-overseas'
+      },
+    '/home-address-overseas':{
+        fields:['address1', 'address2', 'town', 'postcode'],
+        next: '/contact-details-overseas'
+    },
+    '/home-address':{
+        fields:['address1', 'address2', 'town', 'postcode'],
+        next: '/contact-details'
+    },
+    '/contact-details-overseas':{
+        fields:['email','country-code' ,'mobile'],
+        next: '/get-updates-overseas'
+    },
+    '/contact-details':{
+        fields:['email', 'mobile'],
+        next: '/get-updates'
+    },
+    '/get-updates-overseas':{
+        next: '/passport-options'
+    },
+    '/get-updates':{
+        next: '/passport-options'
+    },
+    '/passport-options':{
+        fields: ['passport-options', 'braille'],
+        next: '/sign'
+    },
+    '/sign': {
+        fields: ['can-sign', 'no-sign-reason'],
+        backLink: './',
+        next: '/passport-special-delivery', /* if they are from the UK */
+        controller: require('../../../controllers/go-overseas'),
+        nextAlt: './summary-overseas'
+    },
+    '/summary-overseas':{
+        controller: require('../../../controllers/confirm-overseas'),
+        template: 'confirm',
+        next: '/declaration'
+    },
+    '/passport-special-delivery': {
+        next: '/summary',
+        fields: ['secure-return']
+    },
+    '/summary':{
+        controller: require('../../../controllers/confirm'),
+        template: 'confirm',
+        next: '/declaration'
+    },
+    '/declaration':{
+        next: '/payment'
+    },
+    '/payment':{
+        next: '/processing-payment'
+    },
+    '/processing-payment':{
+        next: '/confirmation'
+    },
+    '/confirmation':{
+        next: '/title'
+    }
+};
