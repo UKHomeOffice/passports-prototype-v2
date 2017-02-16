701d91d59db38836bff61d632019a4e00749f8d0
Rebecca Cottrell <r.e.cottrell@gmail.com>
2017-02-16 16:49:23 +0000

    Add styles for location filter

--- routes/priority_service_170301/intro/index.js
+++ routes/priority_service_170301/intro/index.js
@@ -5,6 +5,6 @@ var app = require('express')(),
 
 app.use(require('hmpo-template-mixins')(fields, { sharedTranslationKey: 'prototype' }));
 
-app.use(wizard(steps, fields, { templatePath: 'priority_service_170215/intro' }));
+app.use(wizard(steps, fields, { templatePath: 'priority_service_170301/intro' }));
 
 module.exports = app;
--- routes/priority_service_170301/not-eligible/index.js
+++ routes/priority_service_170301/not-eligible/index.js
@@ -5,6 +5,6 @@ var app = require('express')(),
 
 app.use(require('hmpo-template-mixins')(fields, { sharedTranslationKey: 'prototype' }));
 
-app.use(wizard(steps, fields, { templatePath: 'priority_service_170215/not-eligible' }));
+app.use(wizard(steps, fields, { templatePath: 'priority_service_170301/not-eligible' }));
 
 module.exports = app;
