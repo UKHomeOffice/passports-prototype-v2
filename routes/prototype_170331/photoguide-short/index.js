750e621e78734ac33cebff8680c49c65be58d9a2
Andreea Ardelean <andreea@Andreeas-MacBook-Pro.local>
2017-02-21 10:54:47 +0000

    Changing the choose your photo method
    - removing the uplaod as a radio, making it a link
    - carrying out the rules on the upload/take your photo page
    - a link fix

--- routes/prototype_170331/photoguide-short/index.js
+++ routes/prototype_170331/photoguide-short/index.js
@@ -5,6 +5,6 @@ var app = require('express')(),
 
 app.use(require('hmpo-template-mixins')(fields, { sharedTranslationKey: 'prototype' }));
 
-app.use(wizard(steps, fields, { templatePath: 'prototype_170123/photoguide-short' }));
+app.use(wizard(steps, fields, { templatePath: 'prototype_170331/photoguide-short' }));
 
 module.exports = app;
