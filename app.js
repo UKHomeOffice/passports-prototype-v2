f1a6c7b1c19a375cc72266b16d2b852c8a622b10
Mia <mia.o'donoghue@digital.homeoffice.com>
2017-03-29 15:26:54 +0100

    238 submission failed
    Added page - content still to do

--- app.js
+++ app.js
@@ -175,6 +175,7 @@ function init(sessionStore) {
             app.use('/prototype_170331/overseas-not-available', require('./routes/prototype_170331/overseas-not-available'));
             app.use('/prototype_170331/throttle', require('./routes/prototype_170331/throttle'));
             app.use('/prototype_170331/payment', require('./routes/prototype_170331/payment'));
+            app.use('/prototype_170331/submission-failed', require('./routes/prototype_170331/submission-failed'));
 
         //prototype 170118
         app.use('/priority_service_170118/overseas', require('./routes/priority_service_170118/overseas'));
