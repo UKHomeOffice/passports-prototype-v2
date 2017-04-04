d9e48d7cdaa5d6c8ab9416e96e0f76b7579a621f
Rebecca Cottrell <r.e.cottrell@gmail.com>
2017-04-04 15:48:19 +0100

    Add new prototype for 19 April user research

---
+++ routes/priority_service_170419/timeout/index.js
@@ -0,0 +1,10 @@
+var app = require('express')(),
+    wizard = require('hmpo-form-wizard'),
+    steps = require('./steps'),
+    fields = require('./fields');
+
+app.use(require('hmpo-template-mixins')(fields, { sharedTranslationKey: 'prototype' }));
+
+app.use(wizard(steps, fields, { templatePath: 'priority_service_170419/timeout' }));
+
+module.exports = app;
