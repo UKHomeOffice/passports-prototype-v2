fdf21fe00aa779fd651a4a84501857d7e91b829f
Andreea Ardelean <andreea@Andreeas-MacBook-Pro.local>
2017-04-19 16:08:14 +0100

    Continue button on index going to success

--- routes/prototype_170331/photo-url/fields.js
+++ routes/prototype_170331/photo-url/fields.js
@@ -1,23 +1,13 @@
 const _ = require('lodash');
-const countries = require('../../../config/countries');
 
 module.exports = {
-'passport-damaged': {
+'photo-url': {
   legend: {
-    value: 'Is your passport damaged?',
+    value: 'URL',
     className: 'visuallyhidden'
   },
-  options: [
-    {value: 'Yes', label: 'Yes'},
-    {value: 'No', label: 'No'}
-  ],
   validate: [
-    'required',
-    {
-      type:'equal',
-      arguments:['No'], /* if Yes is selected */
-      redirect:'https://passportapplication.service.gov.uk/ips-olc/'
-    }
+      'required'
   ]
 }
 };
--- routes/prototype_170331/photo-url/steps.js
+++ routes/prototype_170331/photo-url/steps.js
@@ -1,6 +1,6 @@
 module.exports = {
     '/':{
-      next: '/error'
+      next: '/success'
     },
     '/error': {
         backLink: './',
