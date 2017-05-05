c8553b047ecde8d47167392ac2ece6d2f052c7eb
Andreea Ardelean <andreea@Andreeas-MBP.lan>
2017-05-05 10:28:52 +0100

    Adding the field inputs on the photo code page

--- routes/prototype_oix_170518/intro/fields.js
+++ routes/prototype_oix_170518/intro/fields.js
@@ -33,29 +33,6 @@ module.exports = {
           redirect:'/get-photo-code'
         }
       ]
-    },
-    'choose-photo-overseas': {
-      legend: {
-        value: 'What are you trying to do?',
-        className: 'visuallyhidden'
-      },
-      options: [
-        {value: 'myself', label: 'Show me how to do it myself'},
-        {value: 'upload', label: 'I already have a digital photo'}
-      ],
-      validate: [
-        'required',
-        {
-          type:'equal',
-          arguments:['upload','shop'],
-          redirect:'/../photoguide-short'
-        },
-        {
-          type:'equal',
-          arguments:['upload','upload'],
-          redirect:'/../upload'
-        }
-      ]
     }
 
 };
