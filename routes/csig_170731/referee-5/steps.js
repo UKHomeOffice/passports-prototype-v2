3f776d9b225800bb22738f16e65d35510acc698c
Adrian <a.brereton@kainos.com>
2017-07-21 13:38:11 +0100

    add validation for pex and dob

--- routes/csig_170731/referee-5/steps.js
+++ routes/csig_170731/referee-5/steps.js
@@ -3,6 +3,7 @@ module.exports = {
         next: '/applicant-info'
     },
     '/applicant-info': {
+        fields: ['pex-number','age-day','age-month','age-year'],
         next: '/csig-info'
     },
     '/csig-info': {
