4c8ce3e8d7a411f2c10107e6917ab3b5c6c448ce
Adrian <a.brereton@kainos.com>
2017-07-25 17:53:29 +0100

    move csig referee 5 to single flow

--- routes/csig_170731/referee-5/steps.js
+++ routes/csig_170731/referee-5/steps.js
@@ -4,43 +4,6 @@ module.exports = {
     },
     '/applicant-info': {
         fields: ['pex-number','age-day','age-month','age-year'],
-        next: '/csig-info'
-    },
-    '/csig-info': {
-        fields: ['passport-number','expiry-month','expiry-year', 'phoneno'],
-        next: '/csig-identity-check'
-    },
-    '/csig-identity-check': {
-        fields: ['name', 'lastname','age-day','age-month','age-year','national-insurance'],
-        back:'csig-info',
-        next: '/csig-summary'
-    },
-    '/csig-summary':{
-      next: '/confirm-applicant'
-    },
-    '/confirm-applicant': {
-        fields: ['applicant-check', 'applicant-check-friend', 'applicant-check-address', 'knowntime'],
-        next: '/csig-details-work'
-    },
-    '/csig-details': {
-        fields: ['title'],
-        back:'confirm-applicant',
-        next: '/csig-details-work'
-    },
-    '/csig-details-work': {
-        back:'confirm-applicant',
-        next: '/declaration'
-    },
-    '/declaration': {
-        back:'csig-details-work',
-        next: '/confirmation'
-    },
-    '/confirmation': {
-        back:'declaration',
-        next: '/confirmation'
-    },
-    '/exceptions': {
-
+        next: '/../referee/csig-info/'
     }
-
 };
