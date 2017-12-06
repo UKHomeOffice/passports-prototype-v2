c345c6d73bec4211639264b31b554c854fea03de
Adrian <a.brereton@kainos.com>
2017-12-06 09:37:50 +0000

    fix routing of name change docs pages

--- controllers/change-of-name-docs.js
+++ controllers/change-of-name-docs.js
@@ -8,19 +8,20 @@ var Controller = function() {
 util.inherits(Controller, Base)
 
 Controller.prototype.get = function successHandler(req, res, callback) {
-	if (req.sessionModel.get('change-of-name-reason') == undefined){
+
+	if (req.sessionModel.get('change-name') == false){
 		return res.redirect('./declaration')
 	} else {
 	switch (req.sessionModel.get('change-of-name-reason')) {
-		case 'Marriage':
+		case 'Marriage-or-civil-partnership':
 			return res.redirect('./name-change-docs-for-marriage')
 		case 'Divorce':
 			return res.redirect('./name-change-docs-for-divorce')
 		case 'Small':
 			return res.redirect('./name-change-docs-for-small-changes')
-		case 'Gender':
+		case 'Gender-reassigment':
 			return res.redirect('./name-change-docs-for-gender-change')
-		case 'Other':
+		case 'I-changed-my-name-another-way':
 			return res.redirect('./name-change-docs-for-other-changes')
 	}
 	}
