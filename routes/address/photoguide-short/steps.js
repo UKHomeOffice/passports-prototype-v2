2edf798a39fc761585eb155fbe40f42cd2daeb4d
Maxine <maxinedivers@gmail.com>
2018-01-31 16:41:04 +0000

    New address capture journey

---
+++ routes/address/photoguide-short/steps.js
@@ -0,0 +1,30 @@
+module.exports = {
+  '/': {
+    next: '/short-plain-bg',
+    backLink: '/../address/intro/choose-photo-method'
+  },
+  '/find-a-camera': {
+    next: '/short-plain-bg'
+  },
+  '/short-plain-bg': {
+    next: '/short-lighting'
+  },
+  '/short-lighting': {
+    next: '/short-right-position'
+  },
+  '/short-right-position': {
+      next: '/short-crop'
+  },
+  '/short-crop': {
+    next: '/short-noshadows'
+  },
+  '/short-noshadows': {
+    next: '/short-rules-1'
+  },
+  '/short-rules-1': {
+    next: '/short-rules-2'
+  },
+  '/short-rules-2': {
+    next: '/../takephoto'
+  }
+};
