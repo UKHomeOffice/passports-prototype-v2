fbb6515c1670515243b785d7d458c818e4f763b9
Roderick Tan <rodericktan@users.noreply.github.com>
2018-04-12 13:38:17 +0100

    Nuke FTA Filters View + Routes Replace FTA Filters View + Routes with 12–15s Filters View + Routes

--- routes/ftas/filter-common-temp/index.js
+++ routes/ftas/filter-common-temp/index.js
@@ -7,6 +7,6 @@ app.use(require('hmpo-template-mixins')(fields, { sharedTranslationKey: 'prototy
 
 app.use(wizard(steps, fields, {
   controller: require('../../../controllers/form'),
-  templatePath: 'ftas/filter-common-temp' }));
+  templatePath: 'ftas/filter-temp' }));
 
 module.exports = app;
