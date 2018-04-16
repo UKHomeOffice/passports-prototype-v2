87bd72415884d7759e2fc5117c881af38169593b
Adrian Brereton <a.brereton@kainos.com>
2018-04-16 16:19:16 +0100

    update routing for third party documents|

--- controllers/change-of-name-docs.js
+++ controllers/change-of-name-docs.js
@@ -8,16 +8,15 @@ var Controller = function() {
 util.inherits(Controller, Base)
 
 Controller.prototype.get = function successHandler(req, res, callback) {
-	if (req.sessionModel.get('parental-responsibility') == false) {
-		return res.redirect('./name-change-docs-for-parents')
-	} else if (req.sessionModel.get('16-or-older') == false) {
-		return res.redirect('./documents-thirdparty-under16')
+	if (req.sessionModel.get('parental-responsibility') == false && req.sessionModel.get('relationship-applicant') != "Other") {
+	  return res.redirect('./name-change-docs-for-parents')
+	} else if (req.sessionModel.get('parental-responsibility') == true && req.sessionModel.get('relationship-applicant') != "Other") {
+	  return res.redirect('./declaration')
+	}	else if (req.sessionModel.get('16-or-older') == false && req.sessionModel.get('relationship-applicant') == "Other") {
+	  return res.redirect('./documents-thirdparty-under16')
 	}	else {
-		return res.redirect('./documents-thirdparty-over16')
+	  return res.redirect('./documents-thirdparty-over16')
 	}
 }
-
-// parent no PR > to return res.redirect('./name-change-docs-for-parents')
-
 
 module.exports = Controller
