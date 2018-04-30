cd0fecced0239d28d65b1f8e362fb01cf9e1db2e
Adrian Brereton <a.brereton@kainos.com>
2018-04-30 14:08:25 +0100

    move why are you doing this field to name page

--- controllers/social-worker.js
+++ controllers/social-worker.js
@@ -8,12 +8,17 @@ var Controller = function() {
 util.inherits(Controller, Base)
 
 Controller.prototype.successHandler = function successHandler(req, res, callback) {
-		if (req.sessionModel.get('relationship-applicant') == "Social Worker" &&  req.session['hmpo-wizard-common']['16-or-older'] == false) {
-			req.sessionModel.set('social-worker', true)
-		} else {
-			req.sessionModel.set('social-worker', false)
-		}
-    Base.prototype.successHandler.call(this, req, res, callback);
+  if (req.sessionModel.get('relationship-applicant') == "Social Worker" && req.session['hmpo-wizard-common']['16-or-older'] == false) {
+    req.sessionModel.set('social-worker', true)
+  } else {
+    req.sessionModel.set('social-worker', false)
+  }
+  if (req.sessionModel.get('relationship-applicant') == "Social Worker" || req.sessionModel.get('relationship-applicant') == "Other") {
+    req.sessionModel.set('third-party', true)
+  } else {
+    req.sessionModel.set('third-party', false)
+  }
+  Base.prototype.successHandler.call(this, req, res, callback);
 }
 
 module.exports = Controller;
