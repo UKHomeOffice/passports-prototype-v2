975e37f62692ef58c245f8a1d7abe7ee1f9f0bc5
Roderick Tan <rodericktan@users.noreply.github.com>
2018-05-18 14:21:06 +0100

    DCSRD-122 Create new page: for guidance which can be linked to from anywhere within journeys
    - Create 3 guidance pages

--- routes/guidance/index.js
+++ routes/guidance/index.js
@@ -3,8 +3,14 @@ var app = require('express').Router();
 app.get('/guidance', function (req, res, next) {
     res.render('guidance');
 });
-app.get('/guidance/documents-1', function (req, res, next) {
-    res.render('guidance/documents-1');
+app.get('/guidance/identity-interviews', function (req, res, next) {
+    res.render('guidance/identity-interviews');
+});
+app.get('/guidance/confirming-your-identity', function (req, res, next) {
+    res.render('guidance/confirming-your-identity');
+});
+app.get('/guidance/recognised-professions', function (req, res, next) {
+    res.render('guidance/recognised-professions');
 });
 
 module.exports = app;
