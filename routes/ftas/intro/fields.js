9507e82f932173756d98200b1a688c6c45e470e7
Roderick Tan <rodericktan@users.noreply.github.com>
2018-06-12 11:48:46 +0100

    Reindent lines

--- routes/ftas/intro/fields.js
+++ routes/ftas/intro/fields.js
@@ -1,61 +1,74 @@
 module.exports = {
     'choose-photo': {
-      legend: {
-        value: 'What are you trying to do?',
-        className: 'visuallyhidden'
-      },
-      options: [
-        {value: 'myself', label: 'Show me how to do it myself'},
-        {value: 'shop', label: 'Show me how to get it from a shop (not yet prototyped)'},
-        {value: 'code', label: 'I have a printed photo with a code'},
-        {value: 'upload', label: 'I already have a digital photo'}
-      ],
-      validate: [
-        'required',
-        {
-          type:'equal',
-          arguments:['upload','shop', 'code'],
-          redirect:'/../photoguide-short'
+        legend: {
+            value: 'What are you trying to do?',
+            className: 'visuallyhidden'
         },
-        {
-          type:'equal',
-          arguments:['upload','myself', 'code'],
-          redirect:'/../photoguide-shop'
-        },
-        {
-          type:'equal',
-          arguments:['upload','upload', 'code'],
-          redirect:'/../upload'
-        },
-        {
-          type:'equal',
-          arguments:['upload','myself', 'shop'],
-          redirect:'/get-photo-code'
-        }
-      ]
+        options: [{
+            value: 'myself',
+            label: 'Show me how to do it myself'
+        }, {
+            value: 'shop',
+            label: 'Show me how to get it from a shop (not yet prototyped)'
+        }, {
+            value: 'code',
+            label: 'I have a printed photo with a code'
+        }, {
+            value: 'upload',
+            label: 'I already have a digital photo'
+        }],
+        validate: [
+            'required',
+            {
+                type: 'equal',
+                arguments: [
+                    'upload',
+                    'shop',
+                    'code'
+                ],
+                redirect: '/../photoguide-short'
+            }, {
+                type: 'equal',
+                arguments: ['upload', 'myself', 'code'],
+                redirect: '/../photoguide-shop'
+            }, {
+                type: 'equal',
+                arguments: ['upload', 'upload', 'code'],
+                redirect: '/../upload'
+            }, {
+                type: 'equal',
+                arguments: [
+                    'upload',
+                    'myself',
+                    'shop'
+                ],
+                redirect: '/get-photo-code'
+            }
+        ]
     },
     'choose-photo-overseas': {
-      legend: {
-        value: 'What are you trying to do?',
-        className: 'visuallyhidden'
-      },
-      options: [
-        {value: 'myself', label: 'Show me how to do it myself'},
-        {value: 'upload', label: 'I already have a digital photo'}
-      ],
-      validate: [
-        'required',
-        {
-          type:'equal',
-          arguments:['upload','shop'],
-          redirect:'/../photoguide-short'
+        legend: {
+            value: 'What are you trying to do?',
+            className: 'visuallyhidden'
         },
-        {
-          type:'equal',
-          arguments:['upload','upload'],
-          redirect:'/../upload'
-        }
-      ]
+        options: [{
+            value: 'myself',
+            label: 'Show me how to do it myself'
+        }, {
+            value: 'upload',
+            label: 'I already have a digital photo'
+        }],
+        validate: [
+            'required',
+            {
+                type: 'equal',
+                arguments: ['upload', 'shop'],
+                redirect: '/../photoguide-short'
+            }, {
+                type: 'equal',
+                arguments: ['upload', 'upload'],
+                redirect: '/../upload'
+            }
+        ]
     }
-
 };
--- routes/ftas/intro/steps.js
+++ routes/ftas/intro/steps.js
@@ -8,33 +8,33 @@ module.exports = {
         next: '/what-you-need-overseas'
     },
     '/how-to-apply': {
-      backLink: './',
-      next: '/what-you-need'
+        backLink: './',
+        next: '/what-you-need'
     },
     '/what-you-need': {
-      next: '/photo-retrieved'
+        next: '/photo-retrieved'
     },
     '/you-need-a-photo': {
-      backLink: './what-you-need',
-      next: '/choose-photo-method'
+        backLink: './what-you-need',
+        next: '/choose-photo-method'
     },
     '/choose-photo-method': {
-      fields: ['choose-photo'],
-      next: '/../upload'
+        fields: ['choose-photo'],
+        next: '/../upload'
     },
     '/photo-retrieved': {
-      next: '/../uploadphoto'
+        next: '/../uploadphoto'
     },
     '/get-photo-code': {
-      fields: ['photo-code-photo'],
-      backLink: './choose-photo-method',
-      next: '/retrieving'
+        fields: ['photo-code-photo'],
+        backLink: './choose-photo-method',
+        next: '/retrieving'
     },
     '/retrieving': {
-      backLink: './get-photo-code',
+        backLink: './get-photo-code',
     },
     '/fetch-result': {
-      controller: require('../../../controllers/fetch-result')
+        controller: require('../../../controllers/fetch-result')
     },
     '/error': {
         backLink: './get-photo-code',
