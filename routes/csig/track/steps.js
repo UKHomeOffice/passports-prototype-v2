f684f333d4561ddad323f3b972637d6faab14f72
Adrian Brereton <a.brereton@kainos.com>
2018-06-22 15:16:19 +0100

    duplicate csig renomination flow to track

--- routes/csig/track/steps.js
+++ routes/csig/track/steps.js
@@ -1,9 +1,30 @@
 module.exports = {
     '/': {
-      next: 'csig-pending'
+      next: '/how-to'
+    },
+    '/how-to': {
+        backLink: './',
+        next: '/give-csig-details',
+        controller: require('../../../controllers/csig-email-pre')
+    },
+    '/give-csig-details': {
+      fields: ['csig-email', 'csig-name', 'csig-last-name', 'contact-csig'],
+      backLink: './how-to',
+      next: '/email-sent'
+    },
+    '/email-sent': {
+      controller: require('../../../controllers/csig-email'),
+      next: '/csig-pending'
+    },
+    '/tracking-waiting-renominate': {
+        next: '/track'
+    },
+    '/tracking-waiting-renominate-anytime': {
+        next: '/track'
     },
     '/csig-pending': {
-
+      fields: ['renominate'],
+      next: '/how-to'
     },
     '/csig-complete': {
 
