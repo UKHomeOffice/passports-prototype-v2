e9df5b2f29433c6916331662dfd9f8b10178556e
Roderick Tan <rodericktan@users.noreply.github.com>
2018-07-06 13:08:38 +0100

    Update docs controller to show correct docs for:
    - FTA
    - HFTA
    - Renew
    - CoN

--- controllers/docs-check-required.js
+++ controllers/docs-check-required.js
@@ -9,23 +9,32 @@ util.inherits(Controller, Base)
 
 Controller.prototype.get = function successHandler(req, res, callback) {
 
-	return res.redirect('./docs-fta')
-	// return res.redirect('./docs-renew')
+	if (req.sessionModel.get('passport-before') == false || req.sessionModel.get('old-blue') == true) {
+		return res.redirect('./docs-fta')
+	}
+	if (req.sessionModel.get('passport-before') == true) {
+		if (req.sessionModel.get('change-name') == true) {
+			switch (req.sessionModel.get('change-of-name-reason')) {
+				case 'Marriage-or-civil-partnership':
+					return res.redirect('./docs-name-change-for-marriage-or-civil-partnership')
 
-	// if (req.sessionModel.get('change-name') == false) {
-	// 	return res.redirect('./declaration')
-	// } else {
-	// 	switch (req.sessionModel.get('change-of-name-reason')) {
-	// 		case 'Marriage-or-civil-partnership':
-	// 			return res.redirect('./docs-name-change-for-marriage-or-civil-partnership')
-	// 		case 'Divorce-or-dissolution':
-	// 			return res.redirect('./docs-name-change-for-divorce-or-dissolution')
-	// 		case 'Gender-transition':
-	// 			return res.redirect('./docs-name-change-for-gender-change')
-	// 		case 'I-changed-my-name-another-way':
-	// 			return res.redirect('./docs-name-change-for-other-changes')
-	// 	}
-	// }
+				case 'Divorce-or-dissolution':
+					return res.redirect('./docs-name-change-for-divorce-or-dissolution')
+
+				case 'Gender-transition':
+					return res.redirect('./docs-name-change-for-gender-change')
+
+				case 'I-changed-my-name-another-way':
+					return res.redirect('./docs-name-change-for-other-changes')
+			}
+		}
+		else {
+			return res.redirect('./docs-renew')
+		}
+	}
+	else {
+		return
+	}
 }
 
 module.exports = Controller
