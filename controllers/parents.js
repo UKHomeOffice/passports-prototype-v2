49accd0636cad74cbdc93917021a268eeb78f6ee
Roderick Tan <rodericktan@users.noreply.github.com>
2018-07-12 11:49:43 +0100

    Refactor controller for parents page
    - Refactor and simplify check DOB logic
    - Combine validation and DOB functions

--- controllers/parents.js
+++ controllers/parents.js
@@ -1,4 +1,5 @@
 var util = require('util')
+var moment = require('moment')
 var Base = require('hmpo-form-wizard').Controller
 
 var Controller = function () {
@@ -6,6 +7,21 @@ var Controller = function () {
 }
 
 util.inherits(Controller, Base)
+
+Controller.prototype.getValues = function getValues(req, res, callback) {
+    Base.prototype.getValues.call(this, req, res, (err, values) => {
+        let errorParent1 = values.errors && values.errors['parent1-additional-information'];
+        let errorParent2 = values.errors && values.errors['parent2-additional-information'];
+
+        if (errorParent1 || values['parent1-additional-information']) {
+            values.openParent1AdditionalInfo = 'open';
+        }
+        if (errorParent2 || values['parent2-additional-information']) {
+            values.openParent2AdditionalInfo = 'open';
+        }
+        callback(err, values);
+    });
+}
 
 Controller.prototype.process = function process(req, res, callback) {
     if ( // If ANY of the fields are EMPTY
@@ -42,19 +58,16 @@ Controller.prototype.process = function process(req, res, callback) {
     Base.prototype.process.call(this, req, res, callback);
 }
 
-Controller.prototype.getValues = function getValues(req, res, callback) {
-    Base.prototype.getValues.call(this, req, res, (err, values) => {
-        let errorParent1 = values.errors && values.errors['parent1-additional-information'];
-        let errorParent2 = values.errors && values.errors['parent2-additional-information'];
+Controller.prototype.successHandler = function successHandler(req, res, callback) {
 
-        if (errorParent1 || values['parent1-additional-information']) {
-            values.openParent1AdditionalInfo = 'open';
-        }
-        if (errorParent2 || values['parent2-additional-information']) {
-            values.openParent2AdditionalInfo = 'open';
-        }
-        callback(err, values);
-    });
+    var dobParent1 = moment(req.sessionModel.get('parent1-age-year') + '-' + req.sessionModel.get('parent1-age-month') + '-' + req.sessionModel.get('parent1-age-day'), 'YYYY-MM-DD');
+    var dobParent2 = moment(req.sessionModel.get('parent2-age-year') + '-' + req.sessionModel.get('parent2-age-month') + '-' + req.sessionModel.get('parent2-age-day'), 'YYYY-MM-DD');
+
+    // Check if DOB falls within certain date ranges, then set session variables
+    req.sessionModel.set('parent1-born-before-1983', moment(dobParent1).isBefore('1983-01-01'));
+    req.sessionModel.set('parent2-born-before-1983', moment(dobParent2).isBefore('1983-01-01'));
+
+    Base.prototype.successHandler.call(this, req, res, callback);
 }
 
 module.exports = Controller;
