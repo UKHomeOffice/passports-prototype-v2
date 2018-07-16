3c3ba6e3e62559c74ee78d103c133c000474247a
Adrian Brereton <a.brereton@kainos.com>
2018-07-16 15:10:58 +0100

    add flag for child csig

--- controllers/csig-reference.js
+++ controllers/csig-reference.js
@@ -9,9 +9,13 @@ var Controller = function () {
 util.inherits(Controller, Base);
 
 Controller.prototype.successHandler = function successHandler(req, res, callback) {
+    req.sessionModel.set('csig-child', false);
     var referenceNumber = req.sessionModel.get('pex-number');
     if (referenceNumber.startsWith('2')) return res.redirect('./csig-expired');
     if (referenceNumber.startsWith('3')) return res.redirect('./csig-invalid');
+    if (referenceNumber.startsWith('4')) {
+      req.sessionModel.set('csig-child', true);
+    }
     res.redirect('./declaration');
 
     Base.prototype.successHandler.call(this, req, res, callback);
