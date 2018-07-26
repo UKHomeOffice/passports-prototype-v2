1f2d1f37c00df71e42c26ca193d26f870ccd849b
Adrian Brereton <a.brereton@kainos.com>
2018-07-26 14:41:31 +0100

    revert person applying back to applicant

---
+++ controllers/check-tracking-status.js
@@ -0,0 +1,17 @@
+var util = require('util')
+var Base = require('hmpo-form-wizard').Controller
+
+var Controller = function() {
+	Base.apply(this, arguments)
+}
+
+util.inherits(Controller, Base)
+
+Controller.prototype.get = function (req, res, next) {
+		if (req.query.status) {
+			req.sessionModel.set('tracking-status', req.query.status)
+		}
+    Base.prototype.get.call(this, req, res, next);
+};
+
+module.exports = Controller
