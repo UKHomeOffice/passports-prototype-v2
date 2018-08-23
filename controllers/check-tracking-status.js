9cb3d7ae3ec6679ab6c28d819016135f82276276
Adrian Brereton <a.brereton@kainos.com>
2018-08-23 11:49:16 +0100

    add query for csig type examiner

--- controllers/check-tracking-status.js
+++ controllers/check-tracking-status.js
@@ -14,9 +14,14 @@ Controller.prototype.get = function (req, res, next) {
 
 	req.sessionModel.set('pex', req.query.pex);
 	req.sessionModel.set('tracking-status', '');
+	req.sessionModel.set('csig-type', '');
 	if (req.query.status) {
 		req.sessionModel.set('tracking-status', req.query.status)
 	}
+	if (req.query.csigtype) {
+		req.sessionModel.set('csig-type', req.query.csigtype)
+	}
+	console.log(req.query)
     Base.prototype.get.call(this, req, res, next);
 };
 
