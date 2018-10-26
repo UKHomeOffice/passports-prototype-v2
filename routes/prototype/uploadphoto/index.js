ccb7139f90e54ebf989edc483233925bfddf203f
hvekriya <h.vekriya@kainos.com>
2018-10-26 12:36:20 +0100

    Added in child photo

--- routes/prototype/uploadphoto/index.js
+++ routes/prototype/uploadphoto/index.js
@@ -5,6 +5,6 @@ var app = require('express')(),
 
 app.use(require('hmpo-template-mixins')(fields, { sharedTranslationKey: 'prototype' }));
 
-app.use(wizard(steps, fields, { templatePath: 'prototype/uploadphoto' }));
+app.use(wizard(steps, fields, { templatePath: 'prototype/uploadphoto', name: 'common' }));
 
 module.exports = app;
