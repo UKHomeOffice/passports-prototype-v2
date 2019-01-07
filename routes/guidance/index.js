905fd4f44b6b339ab9dc17bf67a0df11bde7ee50
Roderick Tan <rodericktan@users.noreply.github.com>
2019-01-07 14:08:08 +0000

    Consolidate files
    - Move files from `/guidance/` into `/prototype/help/`
    - Update relative paths and routes

--- routes/guidance/index.js
+++ routes/guidance/index.js
@@ -1,39 +1,8 @@
 var app = require('express').Router();
 
-app.get('/guidance/identity-interviews', function (req, res, next) {
-    res.render('guidance/identity-interviews');
-});
-app.get('/guidance/confirming-your-identity', function (req, res, next) {
-    res.render('guidance/confirming-your-identity');
-});
-app.get('/guidance/recognised-professions', function (req, res, next) {
-    res.render('guidance/recognised-professions');
-});
-app.get('/guidance/british-nationality', function (req, res, next) {
-    res.render('guidance/british-nationality');
-});
-app.get('/guidance/parents-details', function (req, res, next) {
-    res.render('guidance/parents-details');
-});
-app.get('/guidance/additional-documents', function (req, res, next) {
-    res.render('guidance/additional-documents');
-});
-app.get('/guidance/children-acts', function (req, res, next) {
-    res.render('guidance/children-acts');
-});
-app.get('/guidance/prove-responsibility-for-a-child', function (req, res, next) {
-    res.render('guidance/prove-responsibility-for-a-child');
-});
-
 // Brexit ready
 app.get('/guidance/brexit-ready', function (req, res, next) {
     res.render('guidance/brexit-ready');
 });
-
-app.get('/guidance/who-can-confirm', function (req, res, next) {
-    res.render('guidance/who-can-confirm');
-});
-
-
 
 module.exports = app;
--- routes/prototype/help/index.js
+++ routes/prototype/help/index.js
@@ -1,7 +1,34 @@
 var app = require('express')();
 
+app.get('/additional-documents', function (req, res, next) {
+    res.render('prototype/help/additional-documents');
+});
+app.get('/british-nationality', function (req, res, next) {
+    res.render('prototype/help/british-nationality');
+});
+app.get('/children-acts', function (req, res, next) {
+    res.render('prototype/help/children-acts');
+});
+app.get('/confirming-your-identity', function (req, res, next) {
+    res.render('prototype/help/confirming-your-identity');
+});
+app.get('/identity-interviews', function (req, res, next) {
+    res.render('prototype/help/identity-interviews');
+});
+app.get('/parents-details', function (req, res, next) {
+    res.render('prototype/help/parents-details');
+});
 app.get('/photo-rules', function (req, res, next) {
 	res.render('prototype/help/photo-rules');
+});
+app.get('/prove-responsibility-for-a-child', function (req, res, next) {
+    res.render('prototype/help/prove-responsibility-for-a-child');
+});
+app.get('/recognised-professions', function (req, res, next) {
+    res.render('prototype/help/recognised-professions');
+});
+app.get('/who-can-confirm', function (req, res, next) {
+    res.render('prototype/help/who-can-confirm');
 });
 
 module.exports = app;
