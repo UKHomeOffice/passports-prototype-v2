ba8712113e28fa33ac0abcceea7c133419cb5503
Adrian Brereton <a.brereton@kainos.com>
2019-01-21 13:54:02 +0000

    fix court order logic

--- controllers/confirm.js
+++ controllers/confirm.js
@@ -57,8 +57,8 @@ ConfirmForm.prototype.createBreakdown = function (req, values, callback) {
     // If no docs are required for lost and stolen
     if (values['lost-stolen'] == true && 
         values['change-name'] == false && 
-        values['application-for-someone-else'] == false &&
-        values['court-orders-child'] == false
+        values['application-for-someone-else'] == false ||
+        (values['application-for-someone-else'] == true && values['court-orders-child'] == false)
         ) {  
         req.sessionModel.set('lost-stolen-no-docs', true);
     } else {
