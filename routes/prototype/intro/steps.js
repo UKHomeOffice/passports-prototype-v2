94b498f449c5c9cdb63a4f39e301c47cd9e7b8d2
Adrian Brereton <a.brereton@kainos.com>
2019-01-29 16:38:51 +0000

    hide what you'll need for >16

--- routes/prototype/intro/steps.js
+++ routes/prototype/intro/steps.js
@@ -7,7 +7,7 @@ module.exports = {
             condition: function (req, res) {
                 return req.session['hmpo-wizard-common']['passport-before'] == false ||
                     req.session['hmpo-wizard-common']['old-blue'] == true ||
-                    req.session['hmpo-wizard-common']['lost-stolen'] == true
+                    (req.session['hmpo-wizard-common']['lost-stolen'] == true && req.session['hmpo-wizard-common']['16-or-older'] == false)
             }
         }, {
             target: '/what-you-need',
