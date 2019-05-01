36c201b524a115e2faccdc6254a6c96db13e9751
Roderick Tan <rodericktan@users.noreply.github.com>
2019-05-01 11:49:44 +0100

    Update `fetch-photo-result.js` to handle `photo-checker` differently
    - Add variations of photo result page

--- controllers/fetch-photo-result.js
+++ controllers/fetch-photo-result.js
@@ -10,28 +10,59 @@ util.inherits(Controller, Base);
 
 Controller.prototype.get = function successHandler(req, res, callback) {
 
-    if (req.sessionModel.get('choose-photo') == 'take' || req.sessionModel.get('choose-photo') == 'upload') {
-        var photoFileName = req.sessionModel.get('photo-file-name');
+    if (/photo-checker/.test(req.originalUrl)) {
+        if (req.sessionModel.get('choose-photo') == 'take' || req.sessionModel.get('choose-photo') == 'upload') {
+            var photoFileName = req.sessionModel.get('photo-file-name');
 
-        if (photoFileName) {
-            if (photoFileName.endsWith('1')) return res.redirect('./check-and-submit-passed-photo');
-            if (photoFileName.endsWith('2')) return res.redirect('./check-and-submit-photo');
-            if (photoFileName.endsWith('3')) return res.redirect('./not-accepted');
+            if (photoFileName) {
+                if (photoFileName.endsWith('1')) return res.redirect('./check-and-submit-passed-photo');
+                if (photoFileName.endsWith('variation-1')) return res.redirect('./check-and-submit-photo-variation-1');
+                if (photoFileName.endsWith('variation-2')) return res.redirect('./check-and-submit-photo-variation-2');
+                if (photoFileName.endsWith('variation-3')) return res.redirect('./check-and-submit-photo-variation-3');
+                if (photoFileName.endsWith('3')) return res.redirect('./not-accepted');
+            }
+
+            res.redirect('./check-and-submit-passed-photo');
         }
+        else if (req.sessionModel.get('choose-photo') == 'code') {
+            var url = req.sessionModel.get('photo-code-path');
 
-        res.redirect('./check-and-submit-passed-photo');
-    }
-    else if (req.sessionModel.get('choose-photo') == 'code') {
-        var url = req.sessionModel.get('photo-code-path');
+            if (url.startsWith('1')) return res.redirect('./check-and-submit-passed-photo');
+            if (url.startsWith('variation-1')) return res.redirect('./check-and-submit-photo-variation-1');
+            if (url.startsWith('variation-2')) return res.redirect('./check-and-submit-photo-variation-2');
+            if (url.startsWith('variation-3')) return res.redirect('./check-and-submit-photo-variation-3');
+            if (url.startsWith('3')) return res.redirect('./not-accepted');
 
-        if (url.startsWith('1')) return res.redirect('./check-and-submit-passed-photo');
-        if (url.startsWith('2')) return res.redirect('./check-and-submit-photo');
-        if (url.startsWith('3')) return res.redirect('./not-accepted');
-
-        res.redirect('./code-error');
+            res.redirect('./code-error');
+        }
+        else {
+            res.redirect('./check-and-submit-passed-photo');
+        }
     }
     else {
-        res.redirect('./check-and-submit-passed-photo');
+        if (req.sessionModel.get('choose-photo') == 'take' || req.sessionModel.get('choose-photo') == 'upload') {
+            var photoFileName = req.sessionModel.get('photo-file-name');
+
+            if (photoFileName) {
+                if (photoFileName.endsWith('1')) return res.redirect('./check-and-submit-passed-photo');
+                if (photoFileName.endsWith('2')) return res.redirect('./check-and-submit-photo');
+                if (photoFileName.endsWith('3')) return res.redirect('./not-accepted');
+            }
+
+            res.redirect('./check-and-submit-passed-photo');
+        }
+        else if (req.sessionModel.get('choose-photo') == 'code') {
+            var url = req.sessionModel.get('photo-code-path');
+
+            if (url.startsWith('1')) return res.redirect('./check-and-submit-passed-photo');
+            if (url.startsWith('2')) return res.redirect('./check-and-submit-photo');
+            if (url.startsWith('3')) return res.redirect('./not-accepted');
+
+            res.redirect('./code-error');
+        }
+        else {
+            res.redirect('./check-and-submit-passed-photo');
+        }
     }
 };
 
